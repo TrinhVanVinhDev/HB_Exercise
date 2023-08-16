854f5c3af7c9748316f0f430ad74b3c4f7b0a86a
TrinhVanVinhDev <40454155+TrinhVanVinhDev@users.noreply.github.com>
2023-08-16 21:34:15 +0700

    Update Excercise3.cs

--- Buoi4/Assets/Scripts/Excercise3.cs
+++ Buoi4/Assets/Scripts/Excercise3.cs
@@ -5,8 +5,13 @@ using UnityEngine;
 public class Excercise3 : MonoBehaviour
 {
     [SerializeField] private Transform transformObj;
+    [SerializeField] private Transform APoint;
+    [SerializeField] private Transform BPoint;
+    [SerializeField] private Transform CPoint;
+    [SerializeField] private Transform FPoint;
 
     public float speed = 5f;
+    private bool resetPoint = false;
 
     // Start is called before the first frame update
     void Start()
@@ -20,19 +25,30 @@ public class Excercise3 : MonoBehaviour
         //moving to right 3 point
         if (transformObj.transform.position.x <= 5 && transformObj.transform.position.y == 0 && transformObj.transform.position.z == 0)
         {
-            transformObj.transform.position = Vector3.MoveTowards(transform.position, new Vector3(5f, 0f, 0f), speed * Time.deltaTime);
+            transformObj.transform.position = Vector3.MoveTowards(transform.position, APoint.position, speed * Time.deltaTime);
+
+            resetPoint = false;
         }
         if (transformObj.transform.position.x == 5 && transformObj.transform.position.y <= 5 && transformObj.transform.position.z == 0)
         {
-            transformObj.transform.position = Vector3.MoveTowards(transform.position, new Vector3(5f, 5f, 0f), speed * Time.deltaTime);
+            transformObj.transform.position = Vector3.MoveTowards(transform.position, BPoint.position, speed * Time.deltaTime);
         }
         if (transformObj.transform.position.y == 5 && transformObj.transform.position.y == 5 && transformObj.transform.position.z <= 5)
         {
-            transformObj.transform.position = Vector3.MoveTowards(transform.position, new Vector3(5f, 5f, 5f), speed * Time.deltaTime);
+            transformObj.transform.position = Vector3.MoveTowards(transform.position, CPoint.position, speed * Time.deltaTime);
+        }
+        if (resetPoint)
+        {
+            transformObj.transform.position = Vector3.MoveTowards(transform.position, FPoint.position, speed * Time.deltaTime);
         }
         if (transformObj.transform.position.z == 5 && transformObj.transform.position.y == 5 && transformObj.transform.position.z == 5)
         {
-            transformObj.transform.position = new Vector3(0,0,0);
+            resetPoint = true;
         }
+        if (transformObj.transform.position.z == 0 && transformObj.transform.position.y == 0 && transformObj.transform.position.z == 0)
+        {
+            resetPoint = false;
+        }
+
     }
 }
