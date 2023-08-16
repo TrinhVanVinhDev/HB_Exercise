6959c30264116f8e58543129e0b675b74aa81bc9
TrinhVanVinhDev <40454155+TrinhVanVinhDev@users.noreply.github.com>
2023-08-16 21:34:50 +0700

    Update Excercise4.cs

--- Buoi4/Assets/Scripts/Excercise4.cs
+++ Buoi4/Assets/Scripts/Excercise4.cs
@@ -4,25 +4,26 @@ using UnityEngine;
 
 public class Excercise4 : MonoBehaviour
 {
-    public Transform startPoint;
-    public Transform endPoint;
+    [SerializeField] private Transform startPoint;
+    [SerializeField] private Transform endPoint;
+    [SerializeField] private Transform centerPoint;
+
     private float startTime;
-    private float lengthTwoPoint;
     // Start is called before the first frame update
     void Start()
     {
         startTime = Time.time;
-        lengthTwoPoint = Vector3.Distance(startPoint.position, endPoint.position);
     }
 
     // Update is called once per frame
     void Update()
     {
-        Vector3 center = (startPoint.position + endPoint.position) * .5f;
+        Vector3 center = centerPoint.position;
         center -= new Vector3(0, 1, 0);
         Vector3 riseRelCenter = startPoint.position - center;
         Vector3 setRelCenter = endPoint.position - center;
         float fracComplete = (Time.time - startTime) / 1f;
-        transform.position = Vector3.Slerp(riseRelCenter, setRelCenter, fracComplete);
+        transform.position = Vector3.Lerp(riseRelCenter, setRelCenter, fracComplete);
+        transform.position += center;
     }
 }
